ba1cbb57e85e8e86c209140fe307c9a063f1a488
Dima <dmitriygorchanin@gmail.com>
2023-09-09 21:54:03 +0300

    feat: add data annotations to models

--- Warehouse Trainee Task/Models/Department.cs
+++ Warehouse Trainee Task/Models/Department.cs
@@ -1,10 +1,16 @@
-﻿namespace Warehouse_Trainee_Task.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Warehouse_Trainee_Task.Models
 {
     public class Department
     {
+        [Key]
         public required int Id { get; set; }
         public required string Name { get; set; }
+        [ForeignKey("Worker")]
         public int WorkerId { get; set; }
+        [ForeignKey("Product")]
         public int? ProductId { get; set; }
 
         public ICollection<Worker> Workers { get; set; }
--- Warehouse Trainee Task/Models/Worker.cs
+++ Warehouse Trainee Task/Models/Worker.cs
@@ -1,9 +1,15 @@
-﻿namespace Warehouse_Trainee_Task.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Warehouse_Trainee_Task.Models
 {
     public class Worker
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public required string Name { get; set; }
+        [ForeignKey("Department")]
         public int DepartmentId { get; set; }
 
         public ICollection<Department> Departments { get; set; }
