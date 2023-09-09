f5569cd9e5e3b7f32d69d3145772cbbfac2cccee
Dima <dmitriygorchanin@gmail.com>
2023-09-09 22:16:04 +0300

    feat: Add & configure database context

---
+++ Warehouse Trainee Task/Data/WarehouseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Warehouse_Trainee_Task.Models;
+
+namespace Warehouse_Trainee_Task.Data
+{
+    public class WarehouseContext : DbContext
+    {
+        public WarehouseContext(DbContextOptions<WarehouseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Worker>().ToTable(nameof(Worker));
+            modelBuilder.Entity<Department>().ToTable(nameof(Department));
+            modelBuilder.Entity<Product>().ToTable(nameof(Product));
+        }
+    }
+}
