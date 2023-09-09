9612a8d0cb6b12cf14e574005a1b8e3e160fc509
Dima <dmitriygorchanin@gmail.com>
2023-09-09 21:40:48 +0300

    feat: Create database models

---
+++ Warehouse Trainee Task/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace Warehouse_Trainee_Task.Models
+{
+    public class Department
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public int WorkerId { get; set; }
+        public int? ProductId { get; set; }
+
+        public ICollection<Worker> Workers { get; set; }
+        public Product? Product { get; set; }
+    }
+}
---
+++ Warehouse Trainee Task/Models/Worker.cs
@@ -0,0 +1,11 @@
+﻿namespace Warehouse_Trainee_Task.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int DepartmentId { get; set; }
+
+        public ICollection<Department> Departments { get; set; }
+    }
+}
